85d647f0b3073775ea244d36fa65d7cc3db30546
Hampus Nilsson <hampusjojo@gmail.com>
2022-01-16 18:21:36 +0000

    add authentication for likeview

--- forum/views.py
+++ forum/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View, CreateView, ListView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from .forms import AddPost, CommentForm
 from .models import Game, Post, Comment
 from django.urls import reverse_lazy, reverse
 
 
+@login_required
 def LikeView(request, id):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
     liked = False
-    if post.likes.filter(id=request.user.id).exists():
-        post.likes.remove(request.user)
-        liked = False
+    if request.user.is_authenticated:
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+            liked = False
+        else:
+            post.likes.add(request.user)
+            liked = True
+        return redirect(request.META['HTTP_REFERER'])
     else:
-        post.likes.add(request.user)
-        liked = True
-    return redirect(request.META['HTTP_REFERER'])
+        return redirect(request.META['HTTP_REFERER'])
 
 
 class GameList(ListView):
